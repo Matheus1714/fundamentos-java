5283c6541a9f9c4c596d21bb1d2ebd49652207f9
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-11 12:28:45 -0300

    create example exception

--- excecoes/Excecoes.java
+++ excecoes/Excecoes.java
@@ -1,5 +1,20 @@
 package excecoes;
 
+// Existem dois tipos de exceção. As checadas e as não checadas.
+
 public class Excecoes {
-    
+    public static void main(String[] args) {
+        try{
+            validarNumro(10);
+        }catch(Exception e){
+            System.out.println("Deu ruim!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void validarNumro(int numero) throws Exception{
+        if(numero < 100) {
+            throw new Exception("O número é menor que 100");
+        }
+    }
 }
