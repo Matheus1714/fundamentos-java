5c68cfe85714e68f348bb57a4b4b09ef8a2273f7
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-11 12:38:50 -0300

    create constructor examples

---
+++ construtor/TesteDeConstrutor.java
@@ -0,0 +1,9 @@
+package construtor;
+
+public class TesteDeConstrutor {
+    public static void main(String[] args) {
+        Construtor construtor1 = new Construtor(0);
+        Construtor construtor2 = new Construtor(0, "meu texto");
+        
+    }
+}
